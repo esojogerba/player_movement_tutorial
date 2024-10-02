734e2ac1333f669ae9dbc0c8b7300fc0263046e4
esojogerba <esojogerba@users.noreply.github.com>
2024-10-01 23:38:18 -0500

    Added speed variable.

--- main.lua
+++ main.lua
@@ -2,24 +2,25 @@ function love.load()
 	Player = {}
 	Player.x = 400
 	Player.y = 200
+	Player.speed = 3
 end
 
 function love.update(dt)
 	-- Player moves right
 	if love.keyboard.isDown("right") then
-		Player.x = Player.x + 3
+		Player.x = Player.x + Player.speed
 	end
 	-- Player moves left
 	if love.keyboard.isDown("left") then
-		Player.x = Player.x - 3
+		Player.x = Player.x - Player.speed
 	end
 	-- Player moves up
 	if love.keyboard.isDown("up") then
-		Player.y = Player.y - 3
+		Player.y = Player.y - Player.speed
 	end
 	-- Player moves down
 	if love.keyboard.isDown("down") then
-		Player.y = Player.y + 3
+		Player.y = Player.y + Player.speed
 	end
 end
 
