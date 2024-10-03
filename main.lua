7cb53e113fc932e18bcb4ff7a5fac62316d9efd1
esojogerba <esojogerba@users.noreply.github.com>
2024-10-02 21:28:40 -0500

    Player animations added.

--- main.lua
+++ main.lua
@@ -2,6 +2,9 @@ function love.load()
 	-- Anim8 Library
 	anim8 = require("libraries/anim8")
 
+	-- Smooth scaling
+	love.graphics.setDefaultFilter("nearest", "nearest")
+
 	-- Player
 	Player = {}
 	-- Position
@@ -9,9 +12,17 @@ function love.load()
 	Player.y = 200
 	-- Speed
 	Player.speed = 3
-	-- Sprite
-	Player.sprite = love.graphics.newImage("sprites/player-sheet.png")
+	-- Sprite and grid
+	Player.spriteSheet = love.graphics.newImage("sprites/player-sheet.png")
 	Player.grid = anim8.newGrid(12, 18, Player.spriteSheet:getWidth(), Player.spriteSheet:getHeight())
+	-- Animations
+	Player.animations = {}
+	Player.animations.down = anim8.newAnimation(Player.grid("1-4", 1), 0.2)
+	Player.animations.left = anim8.newAnimation(Player.grid("1-4", 2), 0.2)
+	Player.animations.right = anim8.newAnimation(Player.grid("1-4", 3), 0.2)
+	Player.animations.up = anim8.newAnimation(Player.grid("1-4", 4), 0.2)
+	-- Player's current animation
+	Player.anim = Player.animations.left
 
 	-- Background image
 	Background = love.graphics.newImage("sprites/background.png")
@@ -34,11 +45,13 @@ function love.update(dt)
 	if love.keyboard.isDown("down") then
 		Player.y = Player.y + Player.speed
 	end
+
+	Player.anim:update(dt)
 end
 
 function love.draw()
 	-- Draw background
 	love.graphics.draw(Background, 0, 0)
 	-- Draw player sprite
-	love.graphics.draw(Player.sprite, Player.x, Player.y)
+	Player.anim:draw(Player.spriteSheet, Player.x, Player.y, nil, 10)
 end
