8190f5986f13a6e68090ffae531669d2c13dae70
esojogerba <esojogerba@users.noreply.github.com>
2024-11-04 17:32:39 -0600

    Require new player file.

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+require("player")
+
 function love.load()
 	-- Camera library
 	camera = require("libraries/camera")
@@ -16,23 +18,7 @@ function love.load()
 	love.graphics.setDefaultFilter("nearest", "nearest")
 
 	-- Player
-	Player = {}
-	-- Position
-	Player.x = 400
-	Player.y = 200
-	-- Speed
-	Player.speed = 5
-	-- Sprite and grid
-	Player.spriteSheet = love.graphics.newImage("sprites/1_blue_girl.png")
-	Player.grid = anim8.newGrid(18, 22, Player.spriteSheet:getWidth(), Player.spriteSheet:getHeight())
-	-- Animations
-	Player.animations = {}
-	Player.animations.down = anim8.newAnimation(Player.grid("1-4", 1), 0.2)
-	Player.animations.left = anim8.newAnimation(Player.grid("1-4", 2), 0.2)
-	Player.animations.right = anim8.newAnimation(Player.grid("1-4", 3), 0.2)
-	Player.animations.up = anim8.newAnimation(Player.grid("1-4", 4), 0.2)
-	-- Player's current animation
-	Player.anim = Player.animations.down
+	Player:load()
 
 	-- Background image
 	Background = love.graphics.newImage("sprites/background.png")
