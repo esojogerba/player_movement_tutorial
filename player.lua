bb902fd6223ab73bd19787b41e97569e857ee1fa
esojogerba <esojogerba@users.noreply.github.com>
2024-11-07 17:05:56 -0600

    Changed player sprite.

--- player.lua
+++ player.lua
@@ -2,22 +2,27 @@ Player = {}
 
 function Player:load()
 	-- Collider
-	Player.collider = world:newBSGRectangleCollider(400, 200, 80, 100, 10)
+	Player.collider = world:newBSGRectangleCollider(400, 450, 50, 100, 10)
 	Player.collider:setFixedRotation(true)
+
 	-- Position
 	Player.x = 400
-	Player.y = 200
+	Player.y = 450
+
 	-- Speed
 	Player.speed = 5
+
 	-- Sprite and grid
-	Player.spriteSheet = love.graphics.newImage("sprites/1_blue_girl.png")
-	Player.grid = anim8.newGrid(18, 22, Player.spriteSheet:getWidth(), Player.spriteSheet:getHeight())
+	Player.spriteSheet = love.graphics.newImage("sprites/player-sheet.png")
+	Player.grid = anim8.newGrid(12, 18, Player.spriteSheet:getWidth(), Player.spriteSheet:getHeight())
+
 	-- Animations
 	Player.animations = {}
 	Player.animations.down = anim8.newAnimation(Player.grid("1-4", 1), 0.2)
 	Player.animations.left = anim8.newAnimation(Player.grid("1-4", 2), 0.2)
 	Player.animations.right = anim8.newAnimation(Player.grid("1-4", 3), 0.2)
 	Player.animations.up = anim8.newAnimation(Player.grid("1-4", 4), 0.2)
+
 	-- Player's current animation
 	Player.anim = Player.animations.down
 end
